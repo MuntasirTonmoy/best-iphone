041d33aed4ab159bbd46fa8e24b334ac0f614724
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-03-26 07:46:33 +0600

    component file made , favicon added, title change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './components/Header/Header';
+import Phones from './components/Phones/Phones';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Phones></Phones>
     </div>
   );
 }
---
+++ src/components/Phones/Phones.js
@@ -0,0 +1,12 @@
+import './Phones.css'
+import React from 'react';
+
+const Phones = () => {
+    return (
+        <div>
+            <h2>This is phone section</h2>
+        </div>
+    );
+};
+
+export default Phones;

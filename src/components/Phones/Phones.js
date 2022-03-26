2068e8d8a27e7575490e281f9a269fd02c06f859
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-03-26 08:14:18 +0600

    show data in console

--- src/components/Phones/Phones.js
+++ src/components/Phones/Phones.js
@@ -1,9 +1,16 @@
 import './Phones.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Phone from '../Phone/Phone';
 import PhoneSelection from '../PhoneSelection/PhoneSelection';
 
 const Phones = () => {
+    //get data by using useState and useEffect
+    const [phones, setPhones] = useState([]);
+    useEffect( () => {
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }, []);
     return (
         <div>
             <h2>This is phone section</h2>
